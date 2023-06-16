315ae58ec8b7916da0ba5683c969819f7c232f34
Novi Dwi Astuti <noviastuti617@gmail.com>
2023-06-16 23:26:09 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Novi Dwi Astuti" // TODO: replace this
+const studentId = "FE5235050" // TODO: replace this
+
+export { url, studentName, studentId }
